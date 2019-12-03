4d55d5b1258ed06c86cc84a2448da1b06fb5fd8a
LUIS RUANO <luisferuano@gmail.com>
2019-12-03 09:08:24 -0500

    scroll to bottom

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -10,20 +10,24 @@ class MessageList extends React.Component {
   constructor(props) {
     super(props);
     this.card = React.createRef();
-    // setInterval(() => { this.card.current.scrollIntoView({behavior: 'smooth'})}, 2000);
+    // this.card.current && setInterval(() => { this.card.current.scrollIntoView({behavior: 'smooth'})}, 2000);
   }
 
   componentDidMount() {
     //this.scrollToBottom();
   }
 
+  componentDidUpdate(){
+    this.scrollToBottom();
+  }
+
   scrollToBottom = () => {
-    this.card.current.scrollIntoView({behavior: 'smooth'})
+    this.card.current && this.card.current.scrollIntoView({behavior: 'smooth'})
   }
 
   render() {
     const { currentUser, messages } = this.props;
-    // console.log('mess', messages)
+    console.log('mess', this.card)
     if(messages.length === 0) { 
       return (
       <div style={{ display: 'flex',
@@ -40,8 +44,8 @@ class MessageList extends React.Component {
       >
         <ul>
           {messages.map((message, index) => (
-            <div className="divElm" style={{ alignItems: currentUser.id === message.senderId ? 'flex-end':'flex-start' }} key={index}>
-              <div ref={this.card} className="cardDiv" style={{ backgroundColor: currentUser.id === message.senderId ? '#e1f5fe':'#fafafa' }}>
+            <div ref={this.card} className="divElm" style={{ alignItems: currentUser.id === message.senderId ? 'flex-end':'flex-start' }} key={index}>
+              <div  className="cardDiv" style={{ backgroundColor: currentUser.id === message.senderId ? '#e1f5fe':'#fafafa' }}>
                 <Typography className="title" color="textSecondary" gutterBottom>
                   {index > 0 && ((message.senderId === messages[index-1].senderId) || currentUser.id === message.senderId) ? '':message.senderId}
                 </Typography>
